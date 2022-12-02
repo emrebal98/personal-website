573a84ce6299e9635ece2912479186bf068609b7
Emre BAL <59164130+emrebal98@users.noreply.github.com>
2022-12-02 17:21:12 +0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { type NextPage } from 'next';
+import { type AppProps } from 'next/app';
+import { type ReactElement, type ReactNode } from 'react';
+import '../styles/globals.css';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return getLayout(<Component {...pageProps} />);
+};
+
+export default MyApp;
