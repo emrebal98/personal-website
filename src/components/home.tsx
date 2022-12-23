a71197598498e7129f4b0feb4d92cbc546631f52
Emre BAL <59164130+emrebal98@users.noreply.github.com>
2022-12-23 21:16:49 +0300

    styling update

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,20 +1,26 @@
-import { type FunctionComponent } from 'react';
+import { type FunctionComponent, type ReactElement } from 'react';
+
+const Card = ({ children }: { children: ReactElement }) => (
+  <div className="flex w-full flex-col items-center gap-4 rounded bg-gradient-to-br from-slate-400/40 to-slate-400/0 p-8 font-segoeui text-slate-100 md:flex-row md:items-start">
+    <div className="flex flex-col gap-4">{children}</div>
+  </div>
+);
 
 const Home: FunctionComponent = () => (
   <div className="container mx-auto flex flex-col items-center gap-8 py-4">
     {/* Card */}
-    <div className="flex w-full flex-col items-center gap-4 rounded bg-gradient-to-br from-slate-400/40 to-slate-400/0 px-4 py-8 font-segoeui text-slate-100 md:flex-row md:items-start">
-      <div className="flex flex-col gap-4">
-        <h1 className="font-segoeui text-4xl font-bold text-slate-100">Hi, I am Emre BAL</h1>
+    <Card>
+      <>
+        <h1 className="text-4xl font-bold text-slate-100">Hi, I am Emre BAL</h1>
         <h3 className="text-xl font-bold text-slate-100">Full Stack Developer</h3>
         <p className="text-base font-normal text-slate-100">
           I am currently improving myself on Next.js with using T3 stack. You can navigate to skills
           page to see what technologies that I used.
         </p>
-      </div>
-    </div>
-    <div className="flex w-full flex-col items-center gap-4 rounded bg-gradient-to-br from-slate-400/40 to-slate-400/0 px-4 py-8 font-segoeui text-slate-100 md:flex-row md:items-start">
-      <div className="flex flex-col gap-4">
+      </>
+    </Card>
+    <Card>
+      <>
         <h3 className="text-xl font-bold text-slate-100">Why is this site like a code editor?</h3>
         <p className="text-base font-normal text-slate-100">
           Basically, I wanted to create an unusual website to introduce myself.
@@ -31,8 +37,8 @@ const Home: FunctionComponent = () => (
           </li>
           <li>I leave the rest for you to discover. 😀</li>
         </ul>
-      </div>
-    </div>
+      </>
+    </Card>
   </div>
 );
 
