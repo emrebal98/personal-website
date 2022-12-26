72c9b138d8c3584363591b44f41a2940e7769d11
Emre BAL <59164130+emrebal98@users.noreply.github.com>
2022-12-26 16:39:05 +0300

    head tag now renders on serverside as should be

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,6 +6,40 @@ interface LayoutProps {
   children: React.ReactElement;
 }
 
+const SEO = ({ children }: { children: React.ReactElement }) => (
+  <>
+    <Head>
+      {/* Primary Meta Tags */}
+      <title>Emre Bal</title>
+      <meta name="title" content="Emre Bal" />
+      <meta
+        name="description"
+        content="Personal website of Emre Bal. He is a full stack developer. The technologies he used mostly are Next.js, React, TypeScript."
+      />
+      {/* Open Graph / Facebook  */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://emrebal.com/" />
+      <meta property="og:title" content="Emre Bal" />
+      <meta
+        property="og:description"
+        content="Personal website of Emre Bal. He is a full stack developer. The technologies he used mostly are Next.js, React, TypeScript, tRPC, Prisma, MySQL."
+      />
+      <meta property="og:image" content="https://emrebal.com/meta-image.png" />
+      {/* Twitter  */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://emrebal.com/" />
+      <meta property="twitter:title" content="Emre Bal" />
+      <meta
+        property="twitter:description"
+        content="Personal website of Emre Bal. He is a full stack developer. The technologies he used mostly are Next.js, React, TypeScript, tRPC, Prisma, MySQL."
+      />
+      <meta property="twitter:image" content="https://emrebal.com/meta-image.png" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    {children}
+  </>
+);
+
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   const [isHydrated, setIsHydrated] = useState(false);
 
@@ -14,48 +48,18 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
     setIsHydrated(true);
   }, []);
 
-  const page = (
-    <>
-      <Head>
-        {/* Primary Meta Tags */}
-        <title>Emre Bal</title>
-        <meta name="title" content="Emre Bal" />
-        <meta
-          name="description"
-          content="Personal website of Emre Bal. He is a full stack developer. The technologies he used mostly are Next.js, React, TypeScript."
-        />
-        {/* Open Graph / Facebook  */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://emrebal.com/" />
-        <meta property="og:title" content="Emre Bal" />
-        <meta
-          property="og:description"
-          content="Personal website of Emre Bal. He is a full stack developer. The technologies he used mostly are Next.js, React, TypeScript, tRPC, Prisma, MySQL."
-        />
-        <meta property="og:image" content="https://emrebal.com/meta-image.png" />
-        {/* Twitter  */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://emrebal.com/" />
-        <meta property="twitter:title" content="Emre Bal" />
-        <meta
-          property="twitter:description"
-          content="Personal website of Emre Bal. He is a full stack developer. The technologies he used mostly are Next.js, React, TypeScript, tRPC, Prisma, MySQL."
-        />
-        <meta property="twitter:image" content="https://emrebal.com/meta-image.png" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider attribute="class" enableSystem>
-        {children}
-      </ThemeProvider>
-    </>
-  );
-
-  return isHydrated ? (
-    page
-  ) : (
-    <div className="flex min-h-screen items-center justify-center bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
-      <p>Loading...</p>
-    </div>
+  return (
+    <SEO>
+      {isHydrated ? (
+        <ThemeProvider attribute="class" enableSystem>
+          {children}
+        </ThemeProvider>
+      ) : (
+        <div className="flex min-h-screen items-center justify-center bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+          <p>Loading...</p>
+        </div>
+      )}
+    </SEO>
   );
 };
 
